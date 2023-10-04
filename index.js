a795e1104b42b141a13b7bfcb559b6c1ea6411ca
thejjwang <jonathanjacobwang@gmail.com>
2023-10-03 19:19:14 -0700

    connected mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-import express from 'express';
-const app = express()
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+const app = express();
+dotenv.config();
 
-app.listen(5001, ()=>{
-    console.log('connected to backend')
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('connect to mongoDB')
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log('mongoDB disconnected')
 })
+
+mongoose.connection.on("connected", () => {
+    console.log('mongoDB connected')
+})
+
+app.listen(5001, () => {
+  connect();
+  console.log("connected to backend");
+});
