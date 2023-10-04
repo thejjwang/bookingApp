45f1500f633e1f18e082c27f070b9a93a1f30cd8
thejjwang <jonathanjacobwang@gmail.com>
2023-10-03 19:47:10 -0700

    added hotel model

--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from './routes/auth.js'
+import usersRoute from './routes/users.js'
+import hotelsRoute from './routes/hotels.js'
+import roomsRoute from './routes/rooms.js'
+
 const app = express();
 dotenv.config();
 
@@ -19,7 +23,11 @@ mongoose.connection.on("disconnected", () => {
 })
 
 // middleware
-app.use('/auth', authRoute)
+app.use('/api/auth', authRoute)
+app.use('/api/users', authRoute)
+app.use('/api/hotels', authRoute)
+app.use('/api/rooms', authRoute)
+
 
 
 app.listen(5001, () => {
