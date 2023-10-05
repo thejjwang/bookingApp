a72aab0d70f509f114b0402efc028d9184835460
thejjwang <jonathanjacobwang@gmail.com>
2023-10-04 19:15:08 -0700

    added controllers

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,68 +1,21 @@
 import express from 'express';
 import Hotel from '../models/Hotel.js';
-
+import { createHotel, updateHotel, deleteHotel, getHotel, getHotels } from '../controllers/hotel.js';
 const router = express.Router();
 
 // create
-router.post('/', async (req, res) => {
+router.post('/', createHotel);
 
-    const newHotel = new Hotel(req.body)
-    try {
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-    } catch (err) {
-        res.status(500).json(err)
-    }
-})
 // update
-router.put('/:id', async (req, res) => {
-    try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(
-            req.params.id, 
-            {$set: req.body},
-            { new: true}
-            );
-        res.status(200).json(updatedHotel)
-    } catch (err) {
-        res.status(500).json(err)
-    }
-})
+router.put('/:id', updateHotel);
 
 // delete 
-router.delete('/:id', async (req, res) => {
-    try {
-        await Hotel.findByIdAndDelete(
-            req.params.id
-            );
-        res.status(200).json("Hotel has been deleted.")
-    } catch (err) {
-        res.status(500).json(err)
-    }
-})
+router.delete('/:id', deleteHotel);
 
 // get
-router.get('/:id', async (req, res) => {
-    try {
-        const hotel = await Hotel.findById(
-            req.params.id);
-        res.status(200).json(hotel)
-    } catch (err) {
-        res.status(500).json(err)
-    }
-})
+router.get('/:id', getHotel)
 
 // get all
-router.get('/', async (req, res, next) => {
-    const failed = true;
+router.get('/', getHotels)
 
-    if (failed) return next(createError(401, "You are no authenticated"));
-    
-    try {
-        const hotels = await Hotel.findById("asdsd");
-        res.status(200).json(hotels)
-    } catch (err) {
-        next(err)
-    }
-})
-
-export default router 
+export default router;
