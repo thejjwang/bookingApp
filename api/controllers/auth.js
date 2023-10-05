dc21f091eadac0478b9343d25f7d7c7a2fd0c592
thejjwang <jonathanjacobwang@gmail.com>
2023-10-04 21:27:26 -0700

    added cookie parser

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -25,10 +25,13 @@ export const login = async (req,res,next) => {
         if(!user) return next(createError(404, "user not found"))
 
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
-        if(!isPasswordCorrect) 
-        return next(createError(400, "wrong password or username"))
+        if(!isPasswordCorrect) {
+            return next(createError(400, "wrong password or username"))
+        }
+        const token = jwt.sign({id:user._id, isAdmin:user.isAdmin}, process.env.JWT)
+
         const {password, isAdmin, ...otherDetails} = user;
-        res.status(200).json({...otherDetails})
+        res.cookie(200).json({...otherDetails})
     } catch (err) {
         next(err)
     }
